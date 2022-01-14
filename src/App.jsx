b09ff9f31ddbdf95b148c104a9ef52779315102a
Arita Osmani <aritaaosmani@gmail.com>
2022-01-14 20:27:59 +0100

    Categories styling

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,7 @@ function App() {
       </main>
     </>
   );
+
 }
 
 export default App;
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 
 function Category(props) {
-    return <li>
+    return <li className={`category_${props.category.id}`}>
         {/* <!-- Use the Link component from React Router to create
     the anchor tag --> */}
         <Link to={`/categories/${props.category.id}`}>{props.category.name}</Link>
